d2dbfa5a811933e3dea02b6555679697ae8d7958
MuhammedSANJID <sanjid357@gmail.com>
2024-11-23 17:36:07 +0530

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,43 @@
+import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const posts = await client.fetch(STARTUPS_QUERY);
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your startup, <br />
+          Connect with Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p>{query ? `Search results for "${query}"` : "All Startups"}</p>
+
+        <ul className="mt-7 card-grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
+  );
+}
